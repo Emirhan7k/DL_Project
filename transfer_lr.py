4dc8eda41f4095139c06c4d1f2415bd3af7bc5d8
Emirhan Kadan <emirhankadan@gmail.com>
2026-03-19 01:45:05 +0300

    Completed

--- transfer_lr.py
+++ transfer_lr.py
@@ -43,5 +43,55 @@ for i, (image, label) in enumerate(samples):
     axes[i].axis('off')
 plt.show()
 
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
+
+model = models.mobilenet_v2(pretrained=True)
+
+num_ftrs = model.classifier[1].in_features
+model.classifier[1] = nn.Linear(num_ftrs, 102)
+model = model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.classifier[1].parameters(), lr=0.001)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
+
+num_epochs = 3
+
+for epoch in tqdm(range(num_epochs)):
+    model.train()
+    running_loss = 0.0
+    for images, labels in tqdm(train_loader):
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    scheduler.step()
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss/len(train_loader):.4f}')
+
+torch.save(model.state_dict(), 'mobilenet_v2_flowers102.pth')
+
+model.eval()
+all_preds = []
+all_labels = []
+
+
+with torch.no_grad():
+    for images, labels in tqdm(test_loader):
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, preds = torch.max(outputs, 1)
+        all_preds.extend(preds.cpu().numpy())
+        all_labels.extend(labels.cpu().numpy())
+
+cm = confusion_matrix(all_labels, all_preds)
+plt.figure(figsize=(12, 10))
+sns.heatmap(cm, annot=False, cmap='Blues', fmt='g')
+plt.xlabel('Predicted')
+plt.ylabel('True')
+plt.title('Confusion Matrix')
+plt.show()
+print(classification_report(all_labels, all_preds))
